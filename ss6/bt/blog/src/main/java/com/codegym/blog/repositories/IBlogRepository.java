5ea7497d79dd1c211c6e8cfc94ae93a3689a85e3
HieuTBT <trunghieu17082001@gmail.com>
2024-07-22 09:51:15 +0700

    sua bai tap

--- ss6/bt/blog/src/main/java/com/codegym/blog/repositories/IBlogRepository.java
+++ ss6/bt/blog/src/main/java/com/codegym/blog/repositories/IBlogRepository.java
@@ -2,12 +2,10 @@ package com.codegym.blog.repositories;
 
 import com.codegym.blog.models.Blog;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.query.Param;
-
 
 import java.util.List;
 
 public interface IBlogRepository extends JpaRepository<Blog, Integer> {
 
-    List<Blog> findAllByNameContaining(@Param("name") String name);
+    List<Blog> findAllByNameContaining(String name);
 }
