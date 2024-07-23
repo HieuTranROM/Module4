7cb85a18a92115f836b90b4463ad2e577a1f4aa6
HieuTBT <trunghieu17082001@gmail.com>
2024-07-23 09:00:14 +0700

    sua lai loi bt

--- ss6/bt/blog/src/main/java/com/codegym/blog/repositories/ICategoryRepository.java
+++ ss6/bt/blog/src/main/java/com/codegym/blog/repositories/ICategoryRepository.java
@@ -2,10 +2,6 @@ package com.codegym.blog.repositories;
 
 import com.codegym.blog.models.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Service;
 
-import java.util.List;
-
-@Service
 public interface ICategoryRepository extends JpaRepository<Category, Long> {
 }
